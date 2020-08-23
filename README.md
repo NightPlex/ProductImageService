613474659bde36fe8de661305bed296c0fa703fb
Steven <provensoul@gmail.com>
2020-08-23 19:41:07 +0300

    Added heroku properties

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# ProductImageService
+## Requirements
+
+For building and running the application you need:
+
+- [JDK 11](https://www.oracle.com/java/technologies/javase-jdk11-downloads.html)
+- [Maven 4](https://maven.apache.org)
+
+## Running the application locally
+
+There are several ways to run a Spring Boot application on your local machine. One way is to execute the `main` method in the `com\example\productimage\ProductImageColourServiceApplication.java` class from your IDE.
+
+Alternatively you can use the [Spring Boot Maven plugin](https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:
+
+```shell
+mvn spring-boot:run
+```
+
+Lastly, you can use maven to build and test.
+
+```shell
+$ cd productimage
+$ ./mvnw clean install
+```
+
+## Deploying the application to OpenShift
+
+The easiest way to deploy the sample application to OpenShift is to use the [OpenShift CLI](https://docs.openshift.org/latest/cli_reference/index.html):
+
+```shell
+oc new-app codecentric/springboot-maven3-centos~https://github.com/codecentric/springboot-sample-app
+```
+
+This will create:
+
+* An ImageStream called "springboot-maven3-centos"
+* An ImageStream called "springboot-sample-app"
+* A BuildConfig called "springboot-sample-app"
+* DeploymentConfig called "springboot-sample-app"
+* Service called "springboot-sample-app"
+
+If you want to access the app from outside your OpenShift installation, you have to expose the springboot-sample-app service:
--- src/main/java/com/example/productimage/controller/ProductImageColourController.java
+++ src/main/java/com/example/productimage/controller/ProductImageColourController.java
@@ -2,6 +2,9 @@ package com.example.productimage.controller;
 
 import com.example.productimage.dto.response.ProductImageResponse;
 import com.example.productimage.service.ProductImageColourService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +16,20 @@ import javax.annotation.Resource;
  * Built by Steven
  * Github: "https://github.com/NightPlex"
  **/
-@RestController("/product")
+@RestController
 public class ProductImageColourController {
 
     @Resource
     private ProductImageColourService productImageColourService;
 
+    @ApiOperation(value = "View a list up to 5 products with color palettes", response = ProductImageResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved products with palettes"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Generic connection or data missing issue"),
+            @ApiResponse(code = 404, message = "Product with that search term cannot be found")
+    }
+    )
     @GetMapping("/colours")
     public ResponseEntity<?> fetchProductColours(final @RequestParam String searchQuery) {
         final ProductImageResponse productImageColours = productImageColourService.fetchProductImageColours(searchQuery);
